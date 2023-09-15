6c28242e80490ea9cb696d0b4d0bff45c27e53cc
Palikss <144001537+Palikss@users.noreply.github.com>
2023-09-15 11:19:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+from tkinter import
+from tkinter import ttk
+root = Tk()
+root.title("Приложение на Tkinter")
+root.geometry("1280x720")
+label = Label(text="Hi METANIT.COM")
+label.pack()
+root.mainloop()
+btn = ttk.Button(text="Button
